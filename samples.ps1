0b2a6f7fa125d20adf082e04335444ea3e71ba38
Jordan Lake <jlake@pax8.com>
2023-10-18 15:22:15 +0100

    ex1-3

---
+++ samples.ps1
@@ -0,0 +1,12 @@
+# How many functions are available to you in PowerShell?
+Get-Command -CommandType Function | Measure-Object
+
+# Using WMI/CIM, list all operating system properties for the local machine.
+Get-CimInstance Win32_OperatingSystem -ComputerName $env:COMPUTERNAME | Select-Object *
+
+# Ex3.Using WMI/CIM, write a PowerShell expression that will write an object to the pipeline showing the computer name, the last time the computer booted, and its uptime.
+
+Get-CimInstance Win32_OperatingSystem -ComputerName $env:COMPUTERNAME | Select-Object PSComputerName, LastBootUpTime, @{n='Uptime';e={(Get-Date)-$_.LastBootUpTime}}
+
+#ex4. Create a new PSDrive called Docs that maps to your Documents folder.
+New-PSDrive -Name Docs
