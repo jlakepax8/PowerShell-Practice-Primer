f093ab5cde3cc92b2b169e5b5d647665ecb65eae
Jordan Lake <jlake@pax8.com>
2023-10-19 08:57:53 +0100

    Functions chapt 3.11

---
+++ Powershell in Action.ps1
@@ -0,0 +1,11 @@
+function hello {
+    param (
+        $name = "jordan"
+    )
+    "Hello $name, how are you?"    
+}
+
+
+#Current location  3.11 - https://read.readwise.io/new/read/01hd1mfeq6epgd6jf0vyd2v7s2
+
+
